e7c77cc59deae467137a68cd989a4b7927acfeec
shreeztea <sshristitn@gmail.com>
2019-12-12 11:26:46 +0545

    at the end of day 5

--- qna_app/urls.py
+++ qna_app/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from  qna_app import views
-
+from .views import QuestionModelCreateView
 app_name = 'qna'
 
 urlpatterns = [
     path('add/',views.addquestion,name='add'),
     path('read/',views.question,name='read'),
-    path('ans/',views.question,name='ans'),
-    path('vote_qn/',views.question,name='vote_qn'),
-    path('update/<int:id>',views.question,name='update'),
-    path('delete/<int:id>',views.question,name='delete'),
+    # path('ans/',views.answer,name='ans'),
+    path('vote_qn/<int:id>/',views.vote_question,name='vote_qn'),
+    path('update/<int:id>/',views.update_question,name='update'),
+    path('delete/<int:id>/',views.delete_question,name='delete'),
+    path('',QuestionModelCreateView.as_view(),name='qn')
 ]
--- qna_app/views.py
+++ qna_app/views.py
@@ -1,29 +1,68 @@
 from django.shortcuts import render,redirect
-from .models import QuestionModel
-from .forms import QuestionForm
+from .models import QuestionModel,CategoryModel 
+from .forms import QuestionForm # import form from forms.py
 from django.http import HttpResponse
 
+from django.views.generic import CreateView
 
-# Create your views here.
+class QuestionModelCreateView(CreateView):
+    model = QuestionModel
+    fields = '__all__'
+
+def vote_question(request,id):
+    instance = QuestionModel.objects.get(id=id)
+    vote = instance.question_votes + 1
+    instance.question_votes = vote
+    instance.save()
+    return redirect('qna:read')
+
+
+
 def addquestion(request):
     if request.method == "POST":
         form = QuestionForm(request.POST, request.FILES)
         if form.is_valid():
-            try:
+            try: 
                 form.save()
-                return redirect('qna:read')
-
+                return HttpResponse('Submitted')
             except:
-                return HttpResponse('failure')
-
+                return HttpResponse('Failed')
+        
         else:
+            print(form.errors)
             return HttpResponse(form.errors)
-
-    else:
-        form = QuestionForm
-        return render(request,'newquestion.html',{'form':form})
+        
+    else:    
+        # form = QuestionForm
+        category = CategoryModel.objects.all()
+        return render(request,'questionmodel_create.html',{'category':category})
 
 
 def question(requests):
     question = QuestionModel.objects.all()
-    return render(requests,'questions.html',{'question':question})
+    return render(requests,'questionmodel_list.html',{'question':question})
+
+def update_question(request ,id):
+    question = QuestionModel.objects.get(id=id)
+    if request.method == "POST":
+            form = QuestionForm(request.POST, request.FILES, instance=question)
+            print(form)
+            if form.is_valid():
+                try:                    
+                    form.save()
+                    return redirect('qna:read')
+
+                except:
+                    return HttpResponse('failure')
+
+            else:
+                return HttpResponse(form.errors)
+
+    else:
+        form = QuestionForm(instance=question)
+        return render(request,'questionmodel_update.html',{'form':form})
+
+def delete_question(requests,id):
+    question = QuestionModel.objects.get(id=id)
+    question.delete()
+    return redirect('qna:read')
